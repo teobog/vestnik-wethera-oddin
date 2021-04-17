5c9fb4b0bbe0b2239d1e265e22c60850d142eb34
bteodorovych <54107225+psih0fans@users.noreply.github.com>
2021-04-17 13:42:20 +0300

    charts

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import './styles.css';
+import './js/chart'
 
-document.querySelector('.input_btn').addEventListener('click', () => {console.log('odin, dva')})
+
+let getEl = x => document.querySelector(x)
+
+getEl('.input_nav').addEventListener('change', testFunc)
+
+function testFunc (){
+  let city_request = getEl('.input_nav').value
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city_request}&units=metric&appid=9f82154cc61ca06904eb64ae2c103336`).then(response => response.json()).then(data => console.log(data))
+}
+
---
+++ src/js/chart.js
@@ -0,0 +1,63 @@
+// ЕСЛИ ТЫ СЮДА ЗАШЕЛ - НИЧО ТУТ СУКА НЕ ТРОГАЙ БО СЛОМАЕШЬ
+
+import Chart from 'chart.js/auto';
+
+var ctx = document.getElementById('myChart');
+var myChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '— Temperature, C° ',
+      data: [21, 52, 13, 46, 5, 62],
+      backgroundColor: [
+        'rgba(255, 99, 132, 1)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+      ],
+      borderWidth: 1
+    },
+      {
+        label: '— Humidity, % ',
+        data: [75, 38, 49, 10, 111, 55],
+        backgroundColor: [
+          'blue',
+        ],
+        borderColor: [
+          'blue',
+        ],
+        borderWidth: 1
+      },
+      {
+        label: '— Wind Speed, m/s ',
+        data: [133, 14, 165, 161, 17, 168],
+        backgroundColor: [
+          'orange',
+        ],
+        borderColor: [
+          'orange',
+        ],
+        borderWidth: 1
+      },
+      {
+        label: '— Atmosphere Pressure, m/m',
+        data: [73, 20, 1, 2, 53, 41],
+        backgroundColor: [
+          'tomato',
+        ],
+        borderColor: [
+          'tomato',
+        ],
+        borderWidth: 1
+      }
+    ]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
