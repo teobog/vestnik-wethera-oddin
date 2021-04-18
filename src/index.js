ec2c3c59766bd8bb47da075ed421c91b4efd41fa
bteodorovych <54107225+psih0fans@users.noreply.github.com>
2021-04-18 11:44:39 +0300

    icons

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './js/chart'
 
 let getEl = x => document.querySelector(x)
 
-getEl('.input_nav').addEventListener('change', testFunc)
+getEl('.input_nav').addEventListener('change', cityRequest)
 
-function testFunc (){
+function cityRequest (){
   let city_request = getEl('.input_nav').value
   fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city_request}&units=metric&appid=9f82154cc61ca06904eb64ae2c103336`).then(response => response.json()).then(data => console.log(data))
 }
