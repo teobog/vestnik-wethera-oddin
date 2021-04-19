29c5697368e9ff265bfffc8c2c47e50155c127c2
Evgenii77777 <76007615+Evgenii77777@users.noreply.github.com>
2021-04-19 08:52:52 +0300

    немного правок

--- src/js/time.js
+++ src/js/time.js
@@ -1,28 +1,23 @@
-import timeList from '../templates/time.hbs';
-const input = document.querySelector('.input_nav');
-const listTime = document.querySelector('.date_list');
-const date = new Date();
 
-
-// console.log(date);
-const options = {
+const refs = {
+ input : document.querySelector('.input_nav'),
+ listTime : document.querySelector('.date_list'),
+ date : new Date(),
+ options : {
     weekday: 'short',
     day: 'numeric',
     month: 'long',
     hour: '2-digit',
     minute: '2-digit',
     second: '2-digit'
-};
+}
+}
 
 function onTime(e) {
     e.preventDefault();
-    const inputValue = e.target.value;
-    console.log(inputValue);
-
-    const localeUs = date.toLocaleString('EN-en', options);
-    const mass = localeUs.split(',');
-    
-listTime.innerHTML = `
+    const inputValue = e.target.value
+    const mass = refs.date.toLocaleString('EN-en', refs.options).split(',');  
+    refs.listTime.innerHTML = `
         <li>
         <p>${mass[1]}</p>
         </li>
@@ -32,24 +27,20 @@ listTime.innerHTML = `
         <li>
           <p>${mass[2]}</p>
         </li>
-        
      `
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${inputValue}&units=metric&appid=9f82154cc61ca06904eb64ae2c103336`)
         .then(response => response.json())
-        .then(render)
-    
+        .then(render)  
 }
-function render({ sys }) {
-    
-    const sunrise = sys.sunrise
 
+function render({ sys }) { 
+    const sunrise = sys.sunrise
     console.log(sunrise);
     const hours = Math.floor((sunrise % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
     console.log(hours);
     const mins = Math.floor((sunrise % (1000 * 60 * 60)) / (1000 * 60))
     console.log(mins);
     
-
     const sunset = sys.sunset
     console.log(sunset);
     const hour = Math.floor((sunset % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
@@ -57,21 +48,19 @@ function render({ sys }) {
     const min = Math.floor((sunset % (1000 * 60 * 60)) / (1000 * 60))
     console.log(min);
 
-
-    listTime.insertAdjacentHTML('beforeend',
-        `<li>
+    refs.listTime.insertAdjacentHTML('beforeend',
+  `<li>
     <svg class="sunrise_icon">
         <use href="./images/symbol-defs.svg#icon-sunrise"></use>
     </svg>
     <p>${hours}:${mins}</p>
-</li>
-<li>
+  </li>
+  <li>
     <svg class="sunset_icon">
         <use href="./images/symbol-defs.svg#icon-sunset"></use>
     </svg>
     <p>${hour}:${min}</p>
-</li>`)
-       
+  </li>`)    
 }
 
-input.addEventListener('change', onTime);
+refs.input.addEventListener('change', onTime);
