a913cbd8eccb3dd9b2d2711188155ef1f5b50565
Dima Kozitsyn <dmytrokozitsyn@gmail.com>
2021-04-20 11:08:50 +0300

    добаляет 5 дней

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './styles.css';
 import './js/chart'
 import './js/currentCity.js'
+import './js/fiveDays.js'
 
 
 
--- src/js/currentCity.js
+++ src/js/currentCity.js
@@ -1,41 +1,54 @@
 import currentCityTemplate from '../templates/currentCity.hbs';
 import debounce from 'lodash.debounce';
+import fiveDays from './fiveDays'
 
-const ref = {
+
+
+
+ const ref = {
     serchQuery: document.querySelector('.input_nav'),
-    curentWeatherBlock: document.querySelector('.current_city_list')
+     curentWeatherBlock: document.querySelector('.current_city_list'),
+     fiveDaysWeatherBlock: document.querySelector('.five_days_list'),
+    
 };
 ref.serchQuery.addEventListener('input', debounce(cityRequest,500));
 
+export default {ref}
 
 function cityRequest(e) {
     let serchQuery = (e.target.value);
     if (serchQuery != '') {
         fetchCityWeather(serchQuery)
+        fiveDays.fetchfiveDays(serchQuery)
     }
     
-    console.log(serchQuery);
+    
  }
 
+
 function fetchCityWeather(serchQuery) {
+     fiveDays.fetchfiveDays(serchQuery)
 
     return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${serchQuery}&appid=2b30446187616e7178e0cd559ea22385&units=metric`)
         .then(res => res.json()).then(checkQuery).catch(console.log('ошибка'));
+        
 }
 
 
 function checkQuery(data) {
-    console.log(data);
+    // console.log(data);
     if (data.cod === '404') {
         ref.curentWeatherBlock.innerHTML = '';
 
+
     }
     dataProcessing(data)
+   
 
 }
 
 function dataProcessing({ name, sys: { country }, main: { temp, temp_min, temp_max }, weather }) {
-   console.log(name,country,temp, temp_min, temp_max,weather[0].description);
+//    console.log(name,country,temp, temp_min, temp_max,weather[0].description);
 
     let roundTemp = Math.round(temp);
     let roundTemp_min = Math.round(temp_min);
@@ -59,5 +72,5 @@ function dataProcessing({ name, sys: { country }, main: { temp, temp_min, temp_m
 
 
 function renderWeather(name, country, temp, temp_min, temp_max, wearherIcon) {
-        ref.curentWeatherBlock.innerHTML = currentCityTemplate({ name, country, temp, temp_min, temp_max, wearherIcon });
-    }
+    ref.curentWeatherBlock.innerHTML = currentCityTemplate({ name, country, temp, temp_min, temp_max, wearherIcon });
+  }
