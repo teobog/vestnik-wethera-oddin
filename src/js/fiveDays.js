f00fb0129bb89d9fae582937c4eb01c13b14a113
bteodorovych <54107225+psih0fans@users.noreply.github.com>
2021-04-21 12:32:12 +0300

    commit version

--- src/js/fiveDays.js
+++ src/js/fiveDays.js
@@ -1,147 +1,141 @@
-
 import fiveDaysTemplate from '../templates/fiveDays.hbs';
-export default { fetchfiveDays };
 
- const ref = {
-     fiveDaysWeatherBlock: document.querySelector('.five_days_list'),
-    fiveDaysTitle: document.querySelector('.h2_container'),
+export default {fetchfiveDays};
+
+const ref = {
+  fiveDaysWeatherBlock: document.querySelector('.five_days_list'),
+  fiveDaysTitle: document.querySelector('.h2_container'),
 
 };
 
 
 function fetchfiveDays(serchQuery) {
-                      
-    return fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${serchQuery}&appid=33417cdb9f22734d9f99a1eef2dd1402&units=metric`)
-        .then(res => res.json()).then(checkQuery).catch();
+
+  return fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${serchQuery}&appid=33417cdb9f22734d9f99a1eef2dd1402&units=metric`)
+    .then(res => res.json()).then(checkQuery).catch();
 }
 
 function checkQuery(data) {
-    console.log(data);
+  console.log(data);
 
-    if (data.cod === '404') {
-        ref.fiveDaysWeatherBlock.innerHTML = '';
-    }
-    let name = data.city.name;
-    let country = data.city.country;
-    ref.fiveDaysTitle.innerHTML = `<h2 class="five_days_city_title">${name}, <span class="five_days_country_title">${country}</span></h2>`
+  if (data.cod === '404') {
+    ref.fiveDaysWeatherBlock.innerHTML = '';
+  }
+  let name = data.city.name;
+  let country = data.city.country;
+  ref.fiveDaysTitle.innerHTML = `<h2 class="five_days_city_title">${name}, <span class="five_days_country_title">${country}</span></h2>`
 
-    let date1 = 0;
-    let arr = [];
-    let dateNow = new Date;
-    let first = 0;
-        
-    for (let i = 0; i < 8; i += 1) {
-        date1 = +(data.list[i].dt_txt.substr(8, 2));
-        // console.log(date1);
-        
-        if (dateNow.getDate() == date1) {
-            arr.push(data.list[i].main.temp_min);
-            first = arr.length;
-            
-        }
-         
+  let date1 = 0;
+  let arr = [];
+  let dateNow = new Date;
+  let first = 0;
+
+  for (let i = 0; i < 8; i += 1) {
+    date1 = +(data.list[i].dt_txt.substr(8, 2));
+    // console.log(date1);
+
+    if (dateNow.getDate() == date1) {
+      arr.push(data.list[i].main.temp_min);
+      first = arr.length;
+
     }
-    dataProcessing(first, data);
+
+  }
+  dataProcessing(first, data);
 }
+
 let daysArr = [];
 
-function dataProcessing(first, data) {       
+function dataProcessing(first, data) {
 
 
+  let firstDayArr = [];
 
-    let firstDayArr = [];
-   
-    for (let i = 0; i < first; i += 1) {
-        firstDayArr.push(data.list[i].main.temp_min);
-        firstDayArr.push(data.list[i].main.temp_max);
-    }
-    let minFirstDay = Math.round(Math.min(...firstDayArr));
-    let maxFirstDay = Math.round(Math.max(...firstDayArr));
+  for (let i = 0; i < first; i += 1) {
+    firstDayArr.push(data.list[i].main.temp_min);
+    firstDayArr.push(data.list[i].main.temp_max);
+  }
+  let minFirstDay = Math.round(Math.min(...firstDayArr));
+  let maxFirstDay = Math.round(Math.max(...firstDayArr));
 
-    getDates(0,minFirstDay,maxFirstDay)
-    let second = first + 8;
-   
-    let secondtDayArr = [];
-    for (let i = first; i < second; i += 1) {
-        secondtDayArr.push(data.list[i].main.temp_min)
-        secondtDayArr.push(data.list[i].main.temp_max)
-    }
-    let minSecondtDay = Math.round(Math.min(...secondtDayArr));
-    let maxSecondtDay = Math.round(Math.max(...secondtDayArr));
+  getDates(0, minFirstDay, maxFirstDay)
+  let second = first + 8;
 
-    getDates(first,minSecondtDay,maxSecondtDay)
-    first = second + 8;
-   
-    let thirdDayArr = [];
-    for (let i = second; i < first; i += 1) {
-        thirdDayArr.push(data.list[i].main.temp_min)
-        thirdDayArr.push(data.list[i].main.temp_max)
-            
-    }
-    let minThirdDay = Math.round(Math.min(...thirdDayArr));
-    let maxThirdDay = Math.round(Math.max(...thirdDayArr));
-    getDates(second,minThirdDay,maxThirdDay)
-    second = first + 8;
-    
-    let fourthDayArr = [];
-    for (let i = first; i < second; i += 1) {
-        fourthDayArr.push(data.list[i].main.temp_min)
-        fourthDayArr.push(data.list[i].main.temp_max)
-    }
-     let minFourthDay = Math.round(Math.min(...fourthDayArr));
-    let maxFourthDay = Math.round(Math.max(...fourthDayArr));
-    getDates(first,minFourthDay,maxFourthDay)
-    first = second + 8;
-    
-    ;
-    let fifthDayArr = [];
-    for (let i = second; i < first; i += 1) {
-        fifthDayArr.push(data.list[i].main.temp_min)
-        fifthDayArr.push(data.list[i].main.temp_max)
-    }
-    let minFifthDay = Math.round(Math.min(...fifthDayArr));
-    let maxFifthDay = Math.round(Math.max(...fifthDayArr));
-    getDates(second,minFifthDay,maxFifthDay)  
+  let secondtDayArr = [];
+  for (let i = first; i < second; i += 1) {
+    secondtDayArr.push(data.list[i].main.temp_min)
+    secondtDayArr.push(data.list[i].main.temp_max)
+  }
+  let minSecondtDay = Math.round(Math.min(...secondtDayArr));
+  let maxSecondtDay = Math.round(Math.max(...secondtDayArr));
 
-   
+  getDates(first, minSecondtDay, maxSecondtDay)
+  first = second + 8;
 
-    function getDates(index,minT,maxT) {
+  let thirdDayArr = [];
+  for (let i = second; i < first; i += 1) {
+    thirdDayArr.push(data.list[i].main.temp_min)
+    thirdDayArr.push(data.list[i].main.temp_max)
+
+  }
+  let minThirdDay = Math.round(Math.min(...thirdDayArr));
+  let maxThirdDay = Math.round(Math.max(...thirdDayArr));
+  getDates(second, minThirdDay, maxThirdDay)
+  second = first + 8;
+
+  let fourthDayArr = [];
+  for (let i = first; i < second; i += 1) {
+    fourthDayArr.push(data.list[i].main.temp_min)
+    fourthDayArr.push(data.list[i].main.temp_max)
+  }
+  let minFourthDay = Math.round(Math.min(...fourthDayArr));
+  let maxFourthDay = Math.round(Math.max(...fourthDayArr));
+  getDates(first, minFourthDay, maxFourthDay)
+  first = second + 8;
+
+  ;
+  let fifthDayArr = [];
+  for (let i = second; i < first; i += 1) {
+    fifthDayArr.push(data.list[i].main.temp_min)
+    fifthDayArr.push(data.list[i].main.temp_max)
+  }
+  let minFifthDay = Math.round(Math.min(...fifthDayArr));
+  let maxFifthDay = Math.round(Math.max(...fifthDayArr));
+  getDates(second, minFifthDay, maxFifthDay)
+
+
+  function getDates(index, minT, maxT) {
     const date = new Date(data.list[index].dt * 1000);
     const dateName = date.getDate();
-    
+
     const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     const dayName = days[date.getDay()];
-    
-    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-        const monthName = months[date.getMonth()];
-        
-        
-        createObjDay(dateName, dayName, monthName,minT,maxT);
-        
-    }
-    
-    
-    function createObjDay(dateName, dayName, monthName,minT,maxT) {
-        
-         
-        
-            
-        daysArr.push({
-            date: dateName,
-            day: dayName,
-            month: monthName,
-            min: minT,
-            max: maxT,
-            link:dateName
-        });
-            
-             
-        
-     }
 
-    renderWeather(daysArr);
-}
-    
-function renderWeather(daysArr) {
-    ref.fiveDaysWeatherBlock.innerHTML = fiveDaysTemplate(daysArr);
+    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    const monthName = months[date.getMonth()];
+
+
+    createObjDay(dateName, dayName, monthName, minT, maxT);
+
   }
+
+
+  function createObjDay(dateName, dayName, monthName, minT, maxT) {
+    daysArr.push({
+      date: dateName,
+      day: dayName,
+      month: monthName,
+      min: minT,
+      max: maxT,
+      link: dateName
+    });
+  }
+
+  console.log(daysArr)
+  ref.fiveDaysWeatherBlock.innerHTML = ''
+  renderWeather(daysArr);
+}
+
+function renderWeather(daysArr) {
+  ref.fiveDaysWeatherBlock.innerHTML = fiveDaysTemplate(daysArr);
+}
--- src/js/moreInfo.js
+++ src/js/moreInfo.js
@@ -4,7 +4,7 @@ export default { fetchMoreInfo };
 const ref = {
     fiveDaysWeatherBlock: document.querySelector('.five_days_list'),
     moreInfoBlock: document.querySelector('.full_info_list'),
-    
+
 };
 
 ref.fiveDaysWeatherBlock.addEventListener('click', onClick);
@@ -12,32 +12,32 @@ ref.fiveDaysWeatherBlock.addEventListener('click', onClick);
 
 
 function fetchMoreInfo(serchQuery) {
-                      
+
      return fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${serchQuery}&appid=33417cdb9f22734d9f99a1eef2dd1402&units=metric`)
         .then(res => res.json()).then(dataCheck).catch();
-    
+
 }
 
 function dataCheck(data) {
-    
+
     if (data.cod === '404') {
         ref.moreInfoBlock.innerHTML = '';
     }
-    
+
     let date1 = 0;
     let arr = [];
     let dateNow = new Date;
     let first = 0;
         for (let i = 0; i < 8; i += 1) {
         date1 = +(data.list[i].dt_txt.substr(8, 2));
-        
-        
+
+
         if (dateNow.getDate() == date1) {
             arr.push(data.list[i].main.temp_min);
             first = arr.length;
-            
+
         }
-         
+
     }
     dataProcessing(first, data);
 }
@@ -48,7 +48,7 @@ function dataProcessing(first, data) {
 
 
     let firstDayArr = [];
-   
+
     for (let i = 0; i < first; i += 1) {
         firstDayArr.push({
             time: data.list[i].dt_txt.substr(11, 5),
@@ -60,9 +60,9 @@ function dataProcessing(first, data) {
 
         });
     }
-    
+
     let second = first + 8;
-   
+
     let secondDayArr = [];
     for (let i = first; i < second; i += 1) {
         secondDayArr.push({
@@ -76,7 +76,7 @@ function dataProcessing(first, data) {
         });
     }
     first = second + 8;
-   
+
     let thirdDayArr = [];
     for (let i = second; i < first; i += 1) {
         thirdDayArr.push({
@@ -87,10 +87,10 @@ function dataProcessing(first, data) {
             humidity: data.list[i].main.humidity,
             wind: data.list[i].wind.speed,
         });
-            
+
     }
     second = first + 8;
-    
+
     let fourthDayArr = [];
     for (let i = first; i < second; i += 1) {
         fourthDayArr.push({
@@ -104,7 +104,7 @@ function dataProcessing(first, data) {
         });
     }
     first = second + 8;
-    
+
     let fifthDayArr = [];
     for (let i = second; i < first; i += 1) {
         fifthDayArr.push({
@@ -118,9 +118,8 @@ function dataProcessing(first, data) {
         });
     }
        renderMoreInfo(firstDayArr)
-     
+  console.log(fifthDayArr)
 }
-
 function renderMoreInfo(firstDayArr) {
     ref.moreInfoBlock.innerHTML = moreInfoTemplate(firstDayArr);
   }
@@ -132,6 +131,6 @@ function renderMoreInfo(firstDayArr) {
     if (e.target === e.currentTarget) {
     return
     }
-    
+
     console.log(e.target);
 }
